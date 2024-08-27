2ec7825ce7e3f2f466ede3cda13cd32da78a5f8f
Laureano Luna <laureano.luna@est.fi.uncoma.edu.ar>
2024-08-27 16:20:16 -0300

    Detalles

--- front/src/components/page/admin/turnos/AddTurno.tsx
+++ front/src/components/page/admin/turnos/AddTurno.tsx
@@ -1,24 +1,27 @@
+import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
 import React from 'react'
 
 export default function AddTurno() {
   return (
     <div className='py-5'>
-      <h2>Ingresar Turno</h2>
+      <h2 className='text-2xl mb-5 uppercase'>Ingresar Turno</h2>
       <form className='flex flex-col items-center justify-center w-2/4 m-auto p-5 gap-y-5 text-center'>
-        <span className='grid grid-cols-2 gap-2'>
-          <Input className='' type='date' placeholder='HORARIO' />
+        <div className='grid grid-cols-2 gap-2'>
+          <Input className='' type='date' value='2024-08-11' placeholder='HORARIO' />
           <Input type='time' placeholder='HORARIO' />
-        </span>
+        </div>
         <Input placeholder='DNI DEL PACIENTE' />
         <Input disabled placeholder='NOMBRE COMPLETO' />
         <span className='grid grid-cols-3 gap-2'>
           <Input disabled className='col-span-2' placeholder='CONTRATACION' />
           <Input disabled placeholder='N° DE SESION' />
         </span>
-        <Input type='submit' className='cursor-pointer border hover:border-green-400' />
+        <Button type='submit' size={"lg"} className='cursor-pointer border focus:border-green-400 '>
+          Enviar
+        </Button>
 
-      </form>
-    </div>
+    </form>
+    </div >
   )
 }
