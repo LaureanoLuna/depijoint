4afa7174f5908df4b83d1a88ac021fa31069cf10
Laureano Luna <laureano.luna@est.fi.uncoma.edu.ar>
2025-01-23 23:03:43 -0300

    Aplicamos parte de la vista de Paciente

---
+++ front/src/components/page/admin/pacientes/AddPaciente.tsx
@@ -0,0 +1,6 @@
+
+export default function AddPaciente() {
+  return (
+    <div>AddPaciente</div>
+  )
+}
--- front/src/components/page/admin/pacientes/PacienteList.tsx
+++ front/src/components/page/admin/pacientes/PacienteList.tsx
@@ -1,9 +1,26 @@
 
-import React from 'react'
+import { Card, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+import AddPaciente from './AddPaciente'
 
 export default function PacienteList() {
   return (
-    <div>PacienteList</div>
+    <Card className="flex justify-between items-center px-5">
+      <CardHeader>
+        <CardTitle className="text-justify text-xl uppercase flex justify-center items-center gap-5">
+
+          <span>
+            Turnos
+            <CardDescription className="tracking-widest">
+              del dia
+            </CardDescription>
+          </span>
+        </CardTitle>
+      </CardHeader>
+
+      <CardHeader>
+        <AddPaciente />
+      </CardHeader>
+    </Card>
   )
 }
 
