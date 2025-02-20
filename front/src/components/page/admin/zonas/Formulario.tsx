536bda3eee5ccbb951ad9c7790693a8af56dcc1e
LaureanoLuna <laureano.luna@est.fi.uncoma.edu.ar>
2025-02-20 15:03:57 -0300

    creamos el formulario para agregar articulos tipo zona

--- front/src/components/page/admin/zonas/Formulario.tsx
+++ front/src/components/page/admin/zonas/Formulario.tsx
@@ -2,12 +2,13 @@ import InputForm from "@/assets/components/InputForm";
 import Seleccion from "@/assets/components/Seleccion";
 import { LIST_TIPO } from "@/assets/constant/LIST_TIPOZONA";
 import { useDepiJoint } from "@/assets/context/DepiJointContexto";
-import { TipoZona } from "@/assets/interfaces/tipoZona";
 import { Zona } from "@/assets/interfaces/zona";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, Controller } from "react-hook-form";
 
 export default function Formulario() {
   const [tipo, setTipo] = useState<string | undefined>(undefined);
@@ -16,18 +17,26 @@ export default function Formulario() {
     register,
     handleSubmit,
     formState: { errors },
-    reset,
+    control,
+    setValue,
+    watch
+    //reset,
   } = useForm<Zona>();
 
   const onSubmit: SubmitHandler<Zona> = async (data) => {
-    const success = await AddZona(data);
-    if (success) reset();
+    if(!tipo) return 
+    
+    setValue('tipoId',tipo)
+    console.log(watch());
+
+    //const success = await AddZona(data);
+    //if (success) reset();
   };
 
   return (
     <Card className="mt-3 py-4 px-2">
       <form action="" onSubmit={handleSubmit(onSubmit)} className="px-2">
-        <div className="flex items-center gap-4 uppercase ">
+        <div className="flex items-center gap-1 uppercase ">
           <Seleccion
             opciones={LIST_TIPO.map((tipo) => tipo.tipo)}
             titulo="Tipo"
@@ -37,9 +46,9 @@ export default function Formulario() {
             }}
           />
           {tipo && (
-            <span className="tracking-widest italic">
+            <h2 className="tracking-widest italic w-full text-center">
               {LIST_TIPO.find((f) => f.tipo === tipo)?.descripcion}{" "}
-            </span>
+            </h2>
           )}
         </div>
         <InputForm
@@ -47,7 +56,7 @@ export default function Formulario() {
           type="text"
           register={register}
           name="codigo"
-          required = {true}
+          required={true}
           error={errors.codigo}
         />
         <InputForm
@@ -55,7 +64,7 @@ export default function Formulario() {
           type="text"
           register={register}
           name="nombre"
-          required = {true}
+          required={true}
           error={errors.nombre}
         />
         <InputForm
@@ -63,41 +72,96 @@ export default function Formulario() {
           type="text"
           register={register}
           name="descripcion"
-          required = {true}
+          required={true}
           error={errors.descripcion}
         />
-        <InputForm
-          label="Precio"
-          type="money"
-          register={register}
-          name="precio"
-          required = {true}
-          error={errors.precio}
-        />
-        <InputForm
-          label="Tiempo"
-          type="text"
-          register={register}
-          name="codigo"
-          required = {true}
-          error={errors.tiempo}
-        />
-        <InputForm
-          label="Genero"
-          type="radio"
-          register={register}
-          name="genero"
-          required = {true}
-          error={errors.sexo}
-        />
-        <InputForm
-          label="Genero"
-          type="radio"
-          register={register}
-          name="genero"
-          required = {true}
-          error={errors.sexo}
-        />
+        <div className="flex justify-between items-center gap-2">
+          <InputForm
+            label="Precio"
+            type="text"
+            register={register}
+            name="precio"
+            required={true}
+            error={errors.precio}
+          />
+          <InputForm
+            label="Tiempo"
+            type="text"
+            register={register}
+            name="tiempo"
+            required={true}
+            error={errors.tiempo}
+          />
+        </div>
+        <div className="flex items-start justify-around">
+          <Controller
+            name="sexo"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Label htmlFor="contentCheckGeneroZona" className="my-2">
+                Genero
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={"M"}
+                  id="contentCheckGeneroZona"
+                  className="p-2"
+                  {...field}
+                >
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="M" id="checkZonaGM" />
+                    <Label htmlFor="checkZonaGM">Mujer</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="H" id="checkZonaH" />
+                    <Label htmlFor="checkZonaH">Hombre</Label>
+                  </div>
+                </RadioGroup>
+                {errors.sexo && (
+                  <p role="alert" className="text-xs text-red-500">
+                    {errors.sexo.message}
+                  </p>
+                )}
+              </Label>
+            )}
+          />
+          <Controller
+            name="tamaño"
+            control={control}
+            rules={{ required: true }}
+            render={({ field }) => (
+              <Label htmlFor="contentCheckTamaZona" className="my-2">
+                Tamaño
+                <RadioGroup
+                  onValueChange={field.onChange}
+                  defaultValue={"C"}
+                  id="contentCheckTamaZona"
+                  className="p-2"
+                  {...field}
+                >
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="C" id="checkZonaTC" />
+                    <Label htmlFor="checkZonaC">Chico</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="M" id="checkZonaTM" />
+                    <Label htmlFor="checkZonaTM">Medio</Label>
+                  </div>
+                  <div className="flex items-center space-x-2">
+                    <RadioGroupItem value="G" id="checkZonaTG" />
+                    <Label htmlFor="checkZonaTG">Grande</Label>
+                  </div>
+                </RadioGroup>
+              </Label>
+            )}
+          />
+        </div>
+        <Button
+          className="border p-2 rounded-md hover:bg-gray-500 hover:text-black hover:font-semibold w-full mt-5"
+          type="submit"
+        >
+          Cargar
+        </Button>
       </form>
     </Card>
   );
